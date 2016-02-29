2961b0896378d93b2507774ed26e1035e15c1771
Antti Möttönen <antti.moettoenen@gmail.com>
2016-02-29 21:24:23 +0200

    Done with this shit

--- app/src/main/java/com/aware/plugin/template/Plugin.java
+++ app/src/main/java/com/aware/plugin/template/Plugin.java
@@ -1,12 +1,20 @@
 package com.aware.plugin.template;
 
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 
+import com.aware.Accelerometer;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
+import com.aware.providers.Accelerometer_Provider;
 import com.aware.utils.Aware_Plugin;
 
 public class Plugin extends Aware_Plugin {
+    public static double total_acceleration = 0;
 
     @Override
     public void onCreate() {
@@ -23,6 +31,8 @@ public class Plugin extends Aware_Plugin {
         //Activate programmatically any sensors/plugins you need here
         //e.g., Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER,true);
         //NOTE: if using plugin with dashboard, you can specify the sensors you'll use there.
+        Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER, true);
+        Aware.setSetting(this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 600000000);
 
         //Any active plugin/sensor shares its overall context using broadcasts
         CONTEXT_PRODUCER = new ContextProducer() {
@@ -39,6 +49,11 @@ public class Plugin extends Aware_Plugin {
         //DATABASE_TABLES = Provider.DATABASE_TABLES
         //TABLES_FIELDS = Provider.TABLES_FIELDS
         //CONTEXT_URIS = new Uri[]{ Provider.Table_Data.CONTENT_URI }
+
+        IntentFilter broadcastFilter = new IntentFilter();
+        broadcastFilter.addAction(Accelerometer.ACTION_AWARE_ACCELEROMETER);
+        registerReceiver(accelerometerStatusListener, broadcastFilter);
+
 
         //Activate plugin
         Aware.startPlugin(this, "com.aware.plugin.template");
@@ -62,8 +77,18 @@ public class Plugin extends Aware_Plugin {
 
         //Deactivate any sensors/plugins you activated here
         //e.g., Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER, false);
-
+        Aware.setSetting(this, Aware_Preferences.STATUS_BATTERY, false);
         //Stop plugin
         Aware.stopPlugin(this, "com.aware.plugin.template");
     }
+
+    BroadcastReceiver accelerometerStatusListener = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "" + intent.getExtras().get(Accelerometer.EXTRA_DATA));
+            ContentValues values = (ContentValues)intent.getExtras().get(Accelerometer.EXTRA_DATA);
+            total_acceleration = Math.sqrt(Math.pow((double)values.getAsFloat(Accelerometer_Provider.Accelerometer_Data.VALUES_0), 2) + Math.pow((double)values.getAsFloat(Accelerometer_Provider.Accelerometer_Data.VALUES_1), 2) + Math.pow((double)values.getAsFloat(Accelerometer_Provider.Accelerometer_Data.VALUES_2), 2));
+            Log.d(TAG, ""+ total_acceleration);
+        }
+    };
 }
